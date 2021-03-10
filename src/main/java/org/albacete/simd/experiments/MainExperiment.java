6a15306bb31d27a3bd4d7264b1a689bd3cb628f5
JLaborda <jdlsicilia@gmail.com>
2021-03-10 16:15:17 +0100

    Setting MainExperiment.java for galgo

--- src/main/java/org/albacete/simd/experiments/MainExperiment.java
+++ src/main/java/org/albacete/simd/experiments/MainExperiment.java
@@ -1,30 +1,48 @@
 package org.albacete.simd.experiments;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class MainExperiment{
 
+    public static final String EXPERIMENTS_FOLDER = "/home/jorlabs/projects/ParallelBNs-git/ParallelBNs/experiments/galgo/";
+
+
+    // Caos con los argunmentos
     public static void main(String[] args) {
         // Reading arguments
-        String algorithmName = args[0].toLowerCase();
-        String netPath = args[1];
-        String bbddPath = args[2];
-        String testPath = args[3];
+        String netName = args[0].toLowerCase();
+        String algorithmName = args[1].toLowerCase();
+        String netPath = args[2];
+        String bbddPath = args[3];
+        String testPath = args[4];
         int nThreads = 0;
-        int nInterleaving = 0;
+        int nInterleaving = Integer.parseInt(args[5]);
         int maxIterations = 0;
         int seed = 0;
-        if(args.length > 4){
-            nThreads = Integer.parseInt(args[4]);
-            nInterleaving = Integer.parseInt(args[5]);
+        // HC
+        if (args.length == 7){
             maxIterations = Integer.parseInt(args[6]);
-            seed = Integer.parseInt(args[7]);
         }
-        else{
-            seed = Integer.parseInt(args[4]);
+        // Rest of experiments
+        if(args.length == 9){
+            nThreads = Integer.parseInt(args[6]);
+            maxIterations = Integer.parseInt(args[7]);
+            seed = Integer.parseInt(args[8]);
         }
+
+        System.out.print("PARAMS: " );
+        for(String arg: args){
+            System.out.print(arg + ", ");
+        }
+        System.out.println();
+
         Experiment experiment = null;
         switch (algorithmName) {
             case "ges":
-                experiment = new ExperimentGES(netPath, bbddPath, testPath, seed);
+                experiment = new ExperimentGES(netPath, bbddPath, testPath, nInterleaving);
                 break;
             case "pges":
                 experiment = new ExperimentPGES(netPath, bbddPath, testPath, nThreads, maxIterations, nInterleaving, seed);
@@ -46,6 +64,23 @@ public class MainExperiment{
 
         // Running experiment
         experiment.runExperiment();
-        experiment.saveExperiment();
+        //experiment.saveExperiment();
+        try{
+            String results = experiment.getResults();
+            String savePath = EXPERIMENTS_FOLDER  + "experiment_results_" + netName + ".csv";
+            File file = new File(savePath);
+            FileWriter csvWriter = new FileWriter(savePath, true);
+            if(file.length() == 0) {
+                String header = "algorithm, network, bbdd, thread, interleaving, seed, SHD, LL Score, BDeu Score, dfMM, dfMM plus, Total iterations, Total time(s)\n";
+                csvWriter.append(header);
+            }
+            csvWriter.append(results);
+            csvWriter.flush();
+            csvWriter.close();
+            System.out.println("Results saved at: " + savePath);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
