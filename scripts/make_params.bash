e6bc305c0e417033c2f0fee3d198c868e9cc6e18
JLaborda <JorgeDaniel.Laborda@uclm.es>
2022-02-23 12:36:27 +0000

    Updating make_params.bash for new hyperparams

--- scripts/make_params.bash
+++ scripts/make_params.bash
@@ -10,8 +10,7 @@ ENDING_NETWORKS=".xbif";
 #ENDING_BBDD10K="10k.csv";
 #ENDING_BBDD50K="50k.csv";
 PARAMS_FOLDER=${HOME_FOLDER}"res/params/";
-#SAVE_FILE=${PARAMS_FOLDER}"hyperparams.txt"
-SAVE_FILE="/Users/jdls/developer/projects/ParallelBNs/res/params/hyperparams.txt"
+SAVE_FILE="${PARAMS_FOLDER}hyperparams.txt"
 ###################
 # HYPERPARAMETERS #
 ###################
@@ -23,8 +22,6 @@ declare networks=()
 for net in ${net_names[@]}; do
     networks+=(${NETWORKS_FOLDER}$net${ENDING_NETWORKS} )
 done
-
-#echo "${networks[@]}"
 
 #declare -a databases=(
 #    ${BBDD_FOLDER}"alarm"${ENDING_BBDD10K} ${BBDD_FOLDER}"alarm"${ENDING_BBDD50K}
@@ -62,12 +59,11 @@ declare -a nItInterleavings=(5 10 15)
 #maxIterations=250
 
 echo "Saving data into ${SAVE_FILE}"
-
+echo "" > $SAVE_FILE
 # SAVING HYPERPARAMETERS
 for alg in ${algorithms[@]};
 do
-  echo "Hellooooo"
-  for database in ${databses[@]};
+  for database in ${databases[@]};
   do
     for test in ${tests[@]};
     do
@@ -75,69 +71,13 @@ do
       do
         for nItInterleaving in ${nItInterleavings[@]}
         do
-            for seed in ${seeds[@]}
-            do
-              echo $alg $database $test $threads $nItInterleaving $seed >> $SAVE_FILE
-            done
+          for seed in ${seeds[@]}
+          do
+            echo $alg $database $test $threads $nItInterleaving $seed >> $SAVE_FILE
+          done
         done
       done
     done
   done
 done
-
-
-
-# len_nets=${#networks[@]};
-# for((i=0;i<$len_nets;i++))
-# do
-#     netPath=${networks[$i]};
-#     testPath=${tests[$i]};
-#     net_name=${net_names[$i]}
-
-#     # Deleting files of previous params
-#     FILE=${PARAMS_FOLDER}experiments_${net_name}.txt
-#     #if test -f "$FILE"; then
-#     #    rm $FILE
-#     #fi
-
-#     # Creating new File
-#     >${FILE}
-#     echo "Creating experiments for: ${net_name} at: ${FILE}"
-#     #if test -f "$FILE"; then
-#     #    echo "File Created: $FILE"
-#     #fi
-#     # Defining Databases for current network
-#     databases=()
-#     for ending in ${endings[@]}; do
-#         databases+=(${BBDD_FOLDER}${net_name}$ending)
-#     done
-
-#     for dataPath in ${databases[@]};
-#     do
-#         for nthread in ${nThreads[@]};
-#         do
-#             for nItInterleaving in ${nItInterleavings[@]};
-#             do
-#                 for alg in ${algorithms[@]};
-#                 do
-#                     if [ $alg == "ges" ]
-#                     then
-#                         echo ${net_names[$i]} $alg $netPath $dataPath $testPath >> $FILE
-#                     elif [ $alg == "hc" ]
-#                         then
-#                             echo ${net_names[$i]} $alg $netPath $dataPath $testPath $nItInterleaving $maxIterations >> $FILE
-#                     else
-#                         for seed in ${seeds[@]};
-#                         do
-#                             echo ${net_names[$i]} $alg $netPath $dataPath $testPath $nItInterleaving $maxIterations $nthread $seed >> $FILE
-#                         done
-#                     fi
-#                 done
-#             done
-#         done
-#     done
-# done
-
-# # Iterate the string array using for loop
-# # echo "$net_number $bbdd_number $fusion $nThreads $nItInterleaving" #>> experiments.txt
                     
