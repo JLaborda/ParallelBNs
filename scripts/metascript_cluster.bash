3c5579bdc4c25d15b3e611407e95a98637285914
JLaborda <JorgeDaniel.Laborda@uclm.es>
2022-02-24 08:42:51 +0000

    Updating metascript

--- scripts/metascript_cluster.bash
+++ scripts/metascript_cluster.bash
@@ -5,7 +5,7 @@
 #declare -a net_names=("andes" "link" "mildew" "munin" "pigs" "water" "win95pts")
 #declare -a net_names=("alarm", "barley", "child", "insurance", "hailfinder", "hepar2") # Redes pequeñas
 
-declare -a net_names=("mildew", "water", "win95pts") # Redes medianas/pequeñas faltantes
+# declare -a net_names=("mildew", "water", "win95pts") # Redes medianas/pequeñas faltantes
 #"alarm", 
 # declare -a suffixes=("01" "02" "03" "04" "05" "06")
 #PARAMS_FOLDER="res/params/"
@@ -13,24 +13,16 @@ declare -a net_names=("mildew", "water", "win95pts") # Redes medianas/pequeñas 
 #echo "building docker images in all of the nodes of the cluster..."
 #build-image parallelbns /home/jdls/developer/projects/ParallelBNs/Dockerfile
 #echo "build complete"
+PARAMS_FOLDER="/home/jdls/developer/projects/ParallelBNs/res/params"
+declare -a hyperparams=( /home/jdls/developer/projects/ParallelBNs/res/params/*_alarm_*.txt )
+#echo "${hyperparams[@]}"
 
 SCRIPT="/home/jdls/developer/projects/ParallelBNs/scripts/run_experiments_cluster.bash"
-
-for net in ${net_names[@]}; do
-    qsub -J 0-329 -v PARAMS="$net" "$SCRIPT"				  	# 1 Hilo
-    qsub -J 0-329 -v PARAMS="$net" -l nodes=1:ppn=2 "$SCRIPT"    	# 2 Hilos
-    qsub -J 0-329 -v PARAMS="$net" -l nodes=1:ppn=4 "$SCRIPT"    	# 4 Hilos
-    qsub -J 0-329 -v PARAMS="$net" -l nodes=1:ppn=6 "$SCRIPT"    	# 6 Hilos
-    qsub -J 0-329 -v PARAMS="$net" -l nodes=1:ppn=8 "$SCRIPT"    	# 8 Hilos
+#EL BUENO
+for params in ${hyperparams[@]}; do
+    qsub -J 0-329 -v PARAMS="$params" "$SCRIPT"				  	# 1 Hilo
+    qsub -J 0-329 -v PARAMS="$params" -l nodes=1:ppn=2 "$SCRIPT"    	# 2 Hilos
+    qsub -J 0-329 -v PARAMS="$params" -l nodes=1:ppn=4 "$SCRIPT"    	# 4 Hilos
+    qsub -J 0-329 -v PARAMS="$params" -l nodes=1:ppn=6 "$SCRIPT"    	# 6 Hilos
+    qsub -J 0-329 -v PARAMS="$params" -l nodes=1:ppn=8 "$SCRIPT"    	# 8 Hilos
 done
-
-# Previous
-#for net in ${net_names[@]}; do
-#    #for suffix in ${suffixes[@]}; do
-#    PARAMS="/home/jdls/developer/projects/ParallelBNs/res/params/experiments_${net}.txt" #${net}/${net}_params_${suffix}"
-#    LENGTH=$(cat ${PARAMS} | wc -l)
-#    PARAMS="/parallelbns/res/params/experiments_${net}.txt"
-#    #qsub -v PARAMS=${PARAMS} -N "ParallelBNs-${net}" -J 1-$LENGTH -l nodes=1:ppn=8 $SCRIPT
-#    qsub -v PARAMS=${PARAMS} -N "ParallelBNs-${net}" -J 1-$LENGTH $SCRIPT
-#    #done
-#done
