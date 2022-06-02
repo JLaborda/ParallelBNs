05bba43b41741333a800b543910751f5438b445a
pablot99 <46194720+pablot99@users.noreply.github.com>
2022-06-02 18:42:46 +0200

    Update CircularFusion.java

--- src/main/java/consensusBN/circularFusion/CircularFusion.java
+++ src/main/java/consensusBN/circularFusion/CircularFusion.java
@@ -58,14 +58,10 @@ public class CircularFusion {
         // Splitting edges with the clustering algorithm and then adding them to its corresponding index
         clustering.setProblem(this.problem);
         List<Set<Edge>> subsetEdgesList = clustering.generateEdgeDistribution(numberOfThreads, false);
-
-        System.out.println(" Edges list: "+subsetEdgesList+ "\n");
-        
         subsetEdges = new ConcurrentHashMap<>(numberOfThreads);
         for (int i = 0; i < subsetEdgesList.size(); i++) {
             subsetEdges.put(i, subsetEdgesList.get(i));
         }
-        System.out.println(" Edges MAP: "+subsetEdges+ "\n");
     }
 
     private void initializeValuesInResultsMap(){
@@ -81,16 +77,8 @@ public class CircularFusion {
     }
 
     private void addNewSupplier(int index){
-        System.out.println("\n INTENTANDO AÑADIR NEW SUPPLIER  " + index);
         synchronized(arraySynchronizeThreads[index]) {
-            for (int i = 0; i < arrayFinishThreads.length; i++) {
-                System.out.println("FUERA DE WHILE: i " + i + ": " + arrayFinishThreads[i]);
-            }
             while (!arrayFinishThreads[index]) {
-                System.out.println("\n");
-                for (int i = 0; i < arrayFinishThreads.length; i++) {
-                    System.out.println("DENTRO DE WHILE: i " + i + ": " + arrayFinishThreads[i]);
-                }
                 try {
                     arraySynchronizeThreads[index].wait();
                     
@@ -101,7 +89,6 @@ public class CircularFusion {
         CircularFusionSupplier thread;
         Dag initialDag = circularFusionThreadsResults.get(index);
         Dag inputDag = getInputDag(index);
-        System.out.println("\n  NEW SUPPLIER, index " + index + ", Initial DAG edges = " + initialDag.getEdges().size() + ", Input DAG edges = " + inputDag.getEdges().size() + "\n");
         Set<Edge> subset = subsetEdges.get(index);
         thread = new CircularFusionSupplier(problem, initialDag, inputDag, subset, index);
         arrayFinishThreads[index] = false;
@@ -124,19 +111,15 @@ public class CircularFusion {
         // Submitting threads and then adding a listener to add more threads if necessary
         CompletableFuture<CircularDag> submitter = CompletableFuture.supplyAsync(thread, executor);
         submitter.thenAccept(this::listener);
-        System.out.println("Hace el submitThreads");
     }
 
 
     private void listener(CircularDag dag){
-        System.out.println("Entra en el listener");
         updateResults(dag);
         if(!checkConvergence(dag)) {
             if(dag.id == numberOfThreads-1) {
-                System.out.println("DAG " + dag.id + " añadiendo supplier " + 0);
                 addNewSupplier(0);
             } else {
-                System.out.println("DAG " + dag.id + " añadiendo supplier " + (dag.id+1));
                 addNewSupplier(dag.id + 1);
             }
         }
@@ -150,7 +133,6 @@ public class CircularFusion {
 
         synchronized(arraySynchronizeThreads[id]) {
             arrayFinishThreads[id] = true;
-            System.out.println("\n\n LIBERAMOSSSSSSSSSSSS  " + id);
             arraySynchronizeThreads[id].notify();
         }
     }
@@ -158,10 +140,8 @@ public class CircularFusion {
     private boolean checkConvergence(CircularDag dag){
         if(dag.convergence){
             convergenceCounter++;
-            System.out.println("CONVERGEN: " + convergenceCounter + ", total: " + numberOfThreads);
-            
+
             if(convergenceCounter >= numberOfThreads){
-                System.out.println("\n\n\n\nCONVERGEEEE\n\n\n\n");
                 hasConverged.converged();
             }
             return true;
